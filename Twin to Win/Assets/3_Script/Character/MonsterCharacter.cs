948e4b968b65c152568a8a1da3fe8ccd9d6ec906
Hyunso Oh <dhgustn218@gmail.com>
2023-08-31 16:33:18 +0900

    몬스터 만드는중

--- Twin to Win/Assets/3_Script/Character/MonsterCharacter.cs
+++ Twin to Win/Assets/3_Script/Character/MonsterCharacter.cs
@@ -1,28 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MonsterCharacter : Character
 {
+	[SerializeField] private float fAttackDistance;
+	private NavMeshAgent cAgent;
 	private State cStateIdle = new State("Idle");
 	private State cStateMove = new State("Move");
 	private State cStateAttack = new State("Attack");
 	private State cStateDamage = new State("Damage");
 	private State cStateDie = new State("Die");
+
 	private void Awake()
 	{
 		cStateMachine = GetComponent<StateMachine>();
 		cAnimator = GetComponent<Animator>();
+		cAgent = GetComponent<NavMeshAgent>();
 		StateInitializeOnEnter();
 		StateInitializeOnStay();
 		StateInitializeOnExit();
+		ChangeState(cStateIdle);
 	}
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			cAgent.SetDestination(new Vector3(Random.Range(-20f, 20f), 0, Random.Range(-20f, 20f)));
+			ChangeState(cStateMove);
+		}
+	}
+
 	private void StateInitializeOnEnter()
 	{
 		cStateIdle.onEnter = () => {
 			ChangeAnimation(cStateIdle.strStateName);
 		};
 		cStateMove.onEnter = () => {
+			cAgent.isStopped = false;
 			ChangeAnimation(cStateMove.strStateName);
 		};
 		cStateAttack.onEnter = () => {
@@ -37,11 +53,23 @@ public class MonsterCharacter : Character
 	}
 	private void StateInitializeOnStay()
 	{
-
+		cStateMove.onStay = () => {
+			float fDist = Vector3.Distance(transform.position, cAgent.destination);
+			if (fDist <= fAttackDistance)
+			{
+				ChangeState(cStateAttack);
+			}
+		};
 	}
 	private void StateInitializeOnExit()
 	{
-
+		cStateMove.onExit = () => {
+			cAgent.isStopped = true;
+		};
+	}
+	public void ResetState()
+	{
+		ChangeState(cStateIdle);
 	}
 
 	public override void Attack()
@@ -50,7 +78,7 @@ public class MonsterCharacter : Character
 	}
 	public override void ChangeState(State cNextState)
 	{
-
+		cStateMachine.ChangeState(cNextState);
 	}
 	public override void Damage(float fAmount)
 	{
